2a882b7a817faa64cfcc4f755e6577a36fbeec0e
Willwork71 <will@A32i.tech>
2025-05-09 22:40:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -66,4 +66,4 @@ app.layout = html.Div([
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run_server(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
